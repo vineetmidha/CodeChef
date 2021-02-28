979512a287c61ef6bb3461f93860a3941268682a
Vineet Midha <vineet.midha@yahoo.com>
2021-02-28 16:36:02 +0530

    Coprime Range - Lunchtime Feb 2021

---
+++ CoprimeRange.cpp
@@ -0,0 +1,14 @@
+
+// https://www.codechef.com/LTIME93A/problems/COPRIMEX
+
+// Any prime number > 10^6 and <= 10^9 is the answer.
+
+int main()
+{
+    int t;
+    cin >> t;
+  
+    while (t--){
+        cout << "1000003" << endl;
+  }
+}
