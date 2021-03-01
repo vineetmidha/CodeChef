c7930bd0884061cf4ba29a471ff32a2d4641bab2
Vineet Midha <vineet.midha@yahoo.com>
2021-03-01 10:47:29 +0530

    College Life - Starters Feb 2021

--- CollegeLife2.cpp
+++ CollegeLife2.cpp
@@ -1,10 +1,57 @@
 
 // https://www.codechef.com/START1C/problems/COLGLF2
 
-// Got WA
+// Short code - codechef solution
 
 #include <bits/stdc++.h>
+
 using namespace std;
+
+#define ll long long int
+
+int main()
+{
+    int t;
+    cin >> t;
+  
+    while (t--){
+        int seasons;
+        cin >> seasons;
+
+        vector<int> intro;
+
+        for (int i=0; i<seasons; i++)
+        {
+            int x;
+            cin >> x;
+            intro.push_back(x);
+        }
+
+        ll totalSum = 0;
+
+        for (int i=0; i<seasons; i++)
+        {
+            int episodes;
+            cin >> episodes;
+            
+            for (int j=0; j<episodes; j++) {
+                int duration;
+                cin >> duration;
+
+                if (j==0)
+                    totalSum += duration;
+                else
+                    totalSum += duration - intro[i];
+            }
+        }
+
+        cout << totalSum << endl;
+  }
+}
+
+// OR
+
+// Got WA because of using int (instead of long long int) for sum and episum
 
 void solve(vector<int> &intro, vector<vector<int>> &durations){
     long long int sum = 0;
