ab8182d888378fe8ae51be026a635e54e5ac7b1d
Vineet Midha <vineet.midha@yahoo.com>
2021-03-16 22:38:53 +0530

    Starters - Feb 2021 - Chef and Coupons

--- ChefAndCoupon.cpp
+++ ChefAndCoupon.cpp
@@ -4,6 +4,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Solution by Kushagra Nigam - CodeChef
+
+int main()
+{
+    int t;
+    cin >> t;
+  
+    while (t--){
+        int d, c;
+        cin >> d >> c;
+
+        int a1, a2, a3;
+        cin >> a1 >> a2 >> a3;
+        
+        int b1, b2, b3;
+        cin >> b1 >> b2 >> b3;
+
+        int total_cost_wc = a1 + a2 + a3 + b1 + b2 + b3 + 2*d; // without coupon
+
+        int total_cost_c = c + a1 + a2 + a3 + b1 + b2 + b3; // with coupon
+        
+        if (a1+a2+a3 < 150)
+            total_cost_c += d;
+            
+        if (b1+b2+b3 < 150)
+            total_cost_c += d;
+            
+        if (total_cost_c < total_cost_wc)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+  }
+}
+
+// Original solution
+
 void solve(vector<int> a, vector<int> b, int d, int c){
     int costA = a[0]+a[1]+a[2];
     int costB = b[0]+b[1]+b[2];
